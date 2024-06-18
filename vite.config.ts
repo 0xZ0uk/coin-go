438e4865cd0194646412e37ba1a7343aab34fd03
Pedro Santana <pedrosantana@duck.com>
2024-06-18 23:13:53 +0100

    Fix deploy variables

--- vite.config.ts
+++ vite.config.ts
@@ -6,21 +6,21 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 const _plugins = [
-	MillionLint.vite(),
-	react(),
-	tsconfigPaths(),
-	// Allows using self-signed certificates to run the dev server using HTTPS.
-	// https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
-	// basicSsl(),
+  MillionLint.vite(),
+  react(),
+  tsconfigPaths(),
+  // Allows using self-signed certificates to run the dev server using HTTPS.
+  // https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
+  // basicSsl(),
 ];
 
 export default defineConfig({
-	base: "/twa",
-	plugins: _plugins,
-	publicDir: "./public",
-	server: {
-		// Uncomment this line if you want to expose your dev server and access it from the devices
-		// in the same network.
-		// host: true,
-	},
+  base: "/coin-go/",
+  plugins: _plugins,
+  publicDir: "./public",
+  server: {
+    // Uncomment this line if you want to expose your dev server and access it from the devices
+    // in the same network.
+    // host: true,
+  },
 });
