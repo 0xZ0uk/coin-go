fd4e82848243647addf6b972c97e341546f6be9a
Pedro Santana <pedrosantana@duck.com>
2024-06-19 08:32:12 +0100

    Add jackpot bar

--- src/components/Slot/index.tsx
+++ src/components/Slot/index.tsx
@@ -48,21 +48,30 @@ export const Slot: React.FC = () => {
     );
   }, [publicSeed, privateSeed]);
 
+  const win =
+    spinner1Symbols[1] === spinner2Symbols[1] &&
+    spinner1Symbols[1] === spinner3Symbols[1];
+
   React.useEffect(() => {
-    if (
-      spinner1Symbols[1] === spinner2Symbols[1] &&
-      spinner1Symbols[1] === spinner3Symbols[1]
-    ) {
+    if (win) {
       setScore(score + 1);
     }
   }, [spinner1Symbols, spinner2Symbols, spinner3Symbols]);
-
   return (
     <div className="flex flex-col items-center justify-between gap-16">
-      <div className="grid grid-cols-3 p-4 rounded-lg bg-gradient-to-b from-[#563226] to-[#331c17] w-full gap-2">
-        <Spinner symbols={spinner1Symbols} spinning={spinning} />
-        <Spinner symbols={spinner2Symbols} spinning={spinning} />
-        <Spinner symbols={spinner3Symbols} spinning={spinning} />
+      <div className="w-full">
+        <div className="bg-[#563226] w-full p-4 pb-0 rounded-t-lg">
+          <div className="bg-gradient-to-b from-[#f6cd26] to-[#ac6b26] w-full p-4 rounded-lg">
+            <p className="text-center text-white text-xl">
+              {win ? "You win!" : "You lose!"}
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 p-4 rounded-b-lg bg-gradient-to-b from-[#563226] to-[#331c17] w-full gap-2">
+          <Spinner symbols={spinner1Symbols} spinning={spinning} />
+          <Spinner symbols={spinner2Symbols} spinning={spinning} />
+          <Spinner symbols={spinner3Symbols} spinning={spinning} />
+        </div>
       </div>
       <EnergyBar />
     </div>
